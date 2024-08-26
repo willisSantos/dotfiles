8ef0068b796342589a3c11d1de573dc051cdd53a
willis <willisjunior89@gmail.com>
2024-08-26 15:10:53 -0300

    adding which-key package into config

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -1,3 +1,10 @@
 local map = vim.keymap.set
 
 map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
+map("n", "<leader>e", "<cmd>NvimTreeFocus<CR", { desc = "nvimtree focus window" })
+map("n", "<leader>wK", "<cmd>WhichKey <CR>", { desc = "whichkey all keymaps" })
+map("n", "<leader>wk", function ()
+	vim.cmd("WhichKey " .. vim.fn.input "WhichKey: ")
+end, { desc = "whichkey query lookup" })
+map("n", "<leader>/", "gcc", { desc = "Toggle Comment", remap = true })
+map("v", "<leader>/", "gc", { desc = "Toggle comment", remap = true })
